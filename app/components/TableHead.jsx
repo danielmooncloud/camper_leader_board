20dc0c5ec4e98f30e1864a435a0266e55683aab1
dgsousa <sousa.danielg@gmail.com>
2017-03-29 16:59:59 -0700

    refactor

--- app/components/TableHead.jsx
+++ app/components/TableHead.jsx
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 
 
-export default class TableHead extends React.Component{
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<thead className="table-head">
-				<tr>
-					<th className="col1">#</th>
-					<th className="col2">Camper</th>
-					<th className="col3"><a href="#" onClick={this.props.recent}>Points in past 30 days</a></th>
-					<th className="col4"><a href="#" onClick={this.props.allTime}>All Time Points</a></th>
-				</tr>
-			</thead>
-		)
-	}
+ const TableHead = props => {
+	
+	return (
+		<thead className="table-head">
+			<tr>
+				<th className="col1">#</th>
+				<th className="col2">Camper</th>
+				<th className="col3"><a href="#" onClick={props.recent}>Points in past 30 days</a></th>
+				<th className="col4"><a href="#" onClick={props.allTime}>All Time Points</a></th>
+			</tr>
+		</thead>
+	)
 }
 
 TableHead.propTypes = {
-	recent: React.PropTypes.func.isRequired,
-	allTime: React.PropTypes.func.isRequired
+	recent: PropTypes.func.isRequired,
+	allTime: PropTypes.func.isRequired
 }
+
+export default TableHead;
